0dcc73c3468581bde25574350eed31fa1c2b76d2
Patrick <patrickbellot77@gmail.com>
2016-11-28 21:33:54 -0600

    initial commit

--- Polyglot/ViewController.swift
+++ Polyglot/ViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  var words = [String]()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let titleAttributes = [NSFontAttributeName: UIFont(name: "AmericanTypewriter", size: 22)!]
+    navigationController?.navigationBar.titleTextAttributes = titleAttributes
+    title = "POLYGLOT"
+    
+    let defaults = UserDefaults.standard
+    
+    if let savedWords = defaults.object(forKey: "Words") as? [String] {
+      words = savedWords
+    } else {
+      saveInitialValues(to: defaults)
     }
+  }
+  
+  func saveInitialValues(to defaults: UserDefaults) {
+    words.append("bear::l'ours")
+    words.append("camel::le chameau")
+    words.append("cow::la vache")
+    words.append("fox::le renard")
+    words.append("goat::la chevre")
+    words.append("monkey::le singe")
+    words.append("pig::le cochon")
+    words.append("rabbit::le lapin")
+    words.append("sheep::le mouton")
+    
+    defaults.set(words, forKey: "Words")
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+  }
 
 
 }
